7d7d6eed502828b76b63e4a71fa48ccc6bc8083e
Jennifer Lara <3697758@alunouninter.com>
2023-04-14 04:11:20 -0300

    sub

--- funcoes.js
+++ funcoes.js
@@ -2,3 +2,16 @@
 function toggleTheme() {
     document.body.classList.toggle("dark-theme");
 }
+// Script para atualizar a palavra "DEV" na animação
+const wordSwapElement = document.getElementById("wordSwap");
+const words = ["DEV","TECH", "PROG"]; 
+let currentIndex = 0;
+
+function swapWords() {
+    const currentText = wordSwapElement.textContent;
+    const newText = currentText.replace("DEV", words[currentIndex]);
+    wordSwapElement.textContent = newText;
+    currentIndex = (currentIndex + 1) % words.length;
+}
+
+setInterval(swapWords, 3000);
