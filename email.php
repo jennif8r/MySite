3b00946c2ed940369349106731ba25e0632ba054
Jennifer Lara <3697758@alunouninter.com>
2023-04-14 06:48:59 -0300

    sub

--- email.php
+++ email.php
@@ -6,16 +6,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
     
     // Configurar os cabeçalhos do e-mail
-    $to = "contato@jenniferlara.tech"; // Coloque o seu endereço de e-mail aqui
+    $to = "jennifer.laracristo@gmail.com"; // Coloque o seu endereço de e-mail aqui
     $subject = "Formulário de Contato";
     $headers = "From: $name <$email>" . "\r\n" .
         "Reply-To: $email" . "\r\n" .
         "X-Mailer: PHP/" . phpversion();
     
     // Construir o corpo do e-mail
-    $body = "Nome: $name\n";
-    $body .= "E-mail: $email\n";
-    $body .= "Mensagem:\n$message";
+    $body = "name: $name\n";
+    $body .= "email: $email\n";
+    $body .= "message:\n$message";
     
     // Enviar o e-mail
     if (mail($to, $subject, $body, $headers)) {
